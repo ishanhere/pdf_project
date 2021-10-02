408596edc1d42676e6fb7334f8f59fe09e913a77
ishanhere <ishanmehta.im28@gmail.com>
2021-10-02 13:05:12 +0530

    dependencies fixs

--- loveupdfreactapp/src/App.js
+++ loveupdfreactapp/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from './config/theme';
-import Home from './components/pages/Home'
+import React from "react";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./config/theme";
+import Home from "./components/pages/Home";
 
 const App = () => {
-    return (
-        <MuiThemeProvider theme={theme} >
-            <CssBaseline/>
-            <Home />
-        </MuiThemeProvider>
-    );
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Home />
+    </MuiThemeProvider>
+  );
 };
 
 export default App;
